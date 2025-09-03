bf630c5c312e2d14d4aed6fd201d4992b0478685
Jubair Prince <jubairprince444@gmail.com>
2025-09-03 12:05:06 +0000

    added lib/firebase.js and inserted const firebaseConfig = {   apiKey: "A

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,4 +1,3 @@
-
 // Import the functions you need from the SDKs you need
 import { initializeApp, getApp, getApps } from "firebase/app";
 import { getAuth } from "firebase/auth";
@@ -6,13 +5,12 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  "projectId": "glamora-express",
-  "appId": "1:875576601697:web:d4c7032ebf8ad7c49ece20",
-  "storageBucket": "glamora-express.firebasestorage.app",
-  "apiKey": "AIzaSyBz2qm42GzlCSmGxBorpS9mNF8FgvW5fmc",
-  "authDomain": "glamora-express.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "875576601697"
+    apiKey: "AIzaSyBz2qm42GzlCSmGxBorpS9mNF8FgvW5fmc",
+    authDomain: "glamora-express.firebaseapp.com",
+    projectId: "glamora-express",
+    storageBucket: "glamora-express.firebasestorage.app",
+    messagingSenderId: "875576601697",
+    appId: "1:875576601697:web:d4c7032ebf8ad7c49ece20"
 };
 
 // Initialize Firebase
